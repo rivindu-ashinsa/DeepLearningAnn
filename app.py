e3a805b7d6ad5263202a9bb0194cda96c6d8d185
Rivindu Ashinsa <ashinsa.rivindu@gmail.com>
2025-05-23 18:58:08 +0530

    todos

--- app.py
+++ app.py
@@ -15,13 +15,40 @@ def root():
 
 
 @api.get('/todos/{todo_id}')
-def get_todos():
+def get_todos(todo_id: int):
     for todo in all_todos:
         if todo['todo_id'] == todo_id:
             return todo
         
 
 @api.get('/todos')
-def get_all_todos(): 
-    return all_todos    
+def get_all_todos(first_n: int = None):
+    if first_n:
+        return all_todos[:first_n]
+    else:
+        return all_todos    
 
+
+@api.post('/todos')
+def create_todo(new_todo: dict):
+    new_todo['todo_id'] = len(all_todos) + 1
+    all_todos.append(new_todo)
+    return new_todo
+
+
+@api.put('/todos/{todo_id}')
+def update_todo(todo_id: int, updated_todo: dict):
+    for todo in all_todos:
+        if todo['todo_id'] == todo_id:
+            todo.update(updated_todo)
+            return todo
+    return {'message': 'Todo not found'}
+
+
+@api.delete('/todo/{todo_id}')  
+def delete_todo(todo_id: int):
+    for todo in all_todos:
+        if todo['todo_id'] == todo_id:
+            all_todos.remove(todo)
+            return {'message': 'Todo deleted'}
+    return {'message': 'Todo not found'}
